4fcd600b13f7243ace11f701292e64eb870e30ea
Leland McInnes <leland.mcinnes@gmail.com>
2020-06-12 14:06:40 -0400

    Add StreamedPLSA to init

--- enstop/__init__.py
+++ enstop/__init__.py
@@ -1,3 +1,4 @@
 from enstop.plsa import PLSA
+from enstop.streamed_plsa import StreamedPLSA
 from enstop.enstop_ import EnsembleTopics
 from enstop.utils import log_lift, mean_log_lift, coherence, mean_coherence
