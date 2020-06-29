6948d14b4ecef30732496ce75e21cedb884ac332
Leland McInnes <leland.mcinnes@gmail.com>
2020-06-29 14:36:42 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 configuration = {
     "name": "enstop",
-    "version": "0.2.2",
+    "version": "0.2.3",
     "description": "Ensemble topic modelling with pLSA",
     "long_description": readme(),
     "classifiers": [
