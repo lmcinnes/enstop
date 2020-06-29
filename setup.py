09af5c702855782181c8b8bb4550d594c206cb7f
Leland McInnes <leland.mcinnes@gmail.com>
2020-06-29 19:04:08 -0400

    Fix signature

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 configuration = {
     "name": "enstop",
-    "version": "0.2.3",
+    "version": "0.2.4",
     "description": "Ensemble topic modelling with pLSA",
     "long_description": readme(),
     "classifiers": [
